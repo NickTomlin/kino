8481255a41a2fe65cd4a3d44938ae84bb269d6ab
Nick Tomlin <nick.tomlin@gmail.com>
2017-09-29 16:41:53 -0500

    chore(checkpoint): Start spiking out what a native client would look like

---
+++ host/port.js
@@ -0,0 +1,25 @@
+const fs = require('fs')
+const path = require('path')
+const os = require('os')
+const promisfy = require('util.promisify')
+
+const writeFile = promisfy(fs.writeFile)
+const readFile = promisfy(fs.readFile)
+const portFilePath = path.normalize(os.homedir(), '.vim-remote-port')
+const unlink = promisfy(fs.unlink)
+
+const port = {
+  write (port) {
+    return writeFile(portFilePath, port)
+  },
+  read () {
+    return readFile(portFilePath, 'utf8')
+      .then(contents => ({ port: contents }))
+  },
+  remove () {
+    return unlink(portFilePath)
+  },
+  portFilePath
+}
+
+module.exports = port
